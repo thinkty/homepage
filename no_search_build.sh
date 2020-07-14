ac7e827925fd0eef21057c2358242640ccfbab7a
thinkty <thinkty@naver.com>
2020-07-14 10:40:59 +0900

    added/modified build scripts

--- no_search_build.sh
+++ no_search_build.sh
@@ -3,4 +3,4 @@
 # This is a script to build the Jekyll page
 # and push my contents to Algolia index
 
-JEKYLL_ENV=production bundle exec jekyll build && jekyll algolia
+JEKYLL_ENV=production bundle exec jekyll build
