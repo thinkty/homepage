200c3d3f350f991af5857412a90ec1395d9f5e10
thinkty <thinkty@naver.com>
2021-04-11 16:08:46 +0900

    Updated build script

--- script/build.sh
+++ script/build.sh
@@ -3,4 +3,6 @@
 # This is a script to build the Jekyll page
 # and push my contents to Algolia index
 
-JEKYLL_ENV=production bundle exec jekyll build && jekyll algolia
+JEKYLL_ENV=production bundle exec jekyll build
+cowsay "Build complete. Updating Algolia"
+jekyll algolia
