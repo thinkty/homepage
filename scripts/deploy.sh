e689aa081f8b18df335ad0e5e03076a630a9c94c
thinkty <the.thinkty@gmail.com>
2023-05-21 20:22:42 -0400

    Updated deployment script to truly synchronize between the source directory and the destination bucket

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@
 cowsay "Make sure you are not using 'serve' before deployment (issue #1)"
 
 # This is a script to deploy the _site to aws S3
-aws s3 sync _site/ s3://thinkty.net --profile default
+aws s3 sync --delete _site/ s3://thinkty.net --profile default
