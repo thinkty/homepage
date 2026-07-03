03d26808fe331e2b7e5156d625d4e980c4aa8c0d
thinkty <the.thinkty@gmail.com>
2026-07-03 22:52:35 +0900

    Updated serve script

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-bundle exec jekyll serve --trace
+# Ensure rbenv-managed Ruby is available when running the script
+export PATH="$HOME/.rbenv/bin:$PATH"
+if command -v rbenv >/dev/null 2>&1; then
+	eval "$(rbenv init -)"
+	rbenv rehash
+fi
+
+# Start Jekyll with LiveReload and bind to all interfaces
+bundle exec jekyll serve --livereload --host 0.0.0.0
